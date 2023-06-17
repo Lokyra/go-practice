2cfae5b528402cac91b0d0c837e30ef9613175f9
ayoub <ayoubousbia02@gmail.com>
2023-06-17 17:37:16 +0200

    struct in go

---
+++ Struct/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+    email string
+    zipCode int
+}
+
+type person struct {
+    firstName string
+    lastName string
+    contactInfo
+}
+
+
+func main() {
+    jim := person {
+        firstName: "Jim",
+        lastName: "Party",
+        contactInfo: contactInfo {
+            email: "jim@gmail.com",
+            zipCode: 94000,
+        },
+    }
+    jimPointer := &jim
+    jimPointer.updateName("jimmy")
+    jim.print()
+
+    jim.updateName("jimmy")
+    fmt.Printf("%+v", jim)
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+    (*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+    fmt.Printf("%+v", p)
+}
